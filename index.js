508f59294b9cd1ef44bfea88f6a6bfe43ad540bc
Andreas <andreas@massdriver.cloud>
2022-11-14 14:32:12 -0800

    eyo

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const github = require('@actions/github');
 const os = require('os');
+import { HeadersInit} from 'node-fetch';
+import fetch from 'node-fetch';
+import { mkdir } from 'fs';
 
 async function run() {
   try {
-    const version = core.getInput('version');
+    const version = core.getInput('version', {required: false});
     core.info(`Downloading version ${version} of the Massdriver CLI!`);
 
     const owner = "massdriver-cloud";
     const repo = "massdriver-cli";
 
     const token = core.getInput('token', {required: false});
-    core.info(`token: ${token}`);
-
 
     const octokit = github.getOctokit(token);
 
     const { data: versions } = await octokit.rest.repos.getLatestRelease({owner: owner, repo: repo});
-    core.info(`versions: ${versions}`);
+    core.info("versions: ${versions}");
 
     await setup(version);
 
@@ -27,6 +28,41 @@ async function run() {
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function downloadFile(asset_id, token) {
+  const {
+    body,
+    headers: { accept, 'user-agent': userAgent },
+    method,
+    url,
+  } = octokit.request.endpoint('GET /repos/:owner/:repo/releases/assets/:asset_id',
+  {
+    asset_id: asset_id,
+    headers: {
+      accept: 'application/octet-stream',
+    },
+    'massdriver-cloud',
+    'massdriver-cli',
+  });
+  headers = {
+    accept,
+    authorization: `token ${token}`,
+  };
+  if (typeof userAgent !== 'undefined')
+    headers = { ...headers, 'user-agent': userAgent };
+
+  const response = await fetch(url, { body, headers, method});
+  if (!response.ok) {
+    const text = await response.text();
+    core.warning(text);
+    throw new Error('Invalid response');
+  }
+
+  const pathToCLIZip = await tc.downloadUrl(url, headers);
+  const pathToCLI = await tc.extractZip(pathToCLIZip);
+
+  return pathToCLI;
 }
 
 async function setup(version) {
