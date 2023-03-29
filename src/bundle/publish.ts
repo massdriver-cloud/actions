b2bcd763effc9988a8cd3a74ba9422358f172398
Andreas Kasprzok <andreas@massdriver.cloud>
2023-03-29 09:40:20 -0700

    publish action (#94)
    * publish action
    
    * remove unnecessary comment
    
    * update dist
    
    * s/build/publish

---
+++ src/bundle/publish.ts
@@ -0,0 +1,17 @@
+import core from '@actions/core'
+import exec from '@actions/exec'
+
+const run = async (): Promise<void> => {
+  try {
+    const command = `mass bundle publish`
+    await exec.exec(command)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    core.setFailed(error.message)
+  }
+  return
+}
+
+run()
+
+export default run
