327aa6e71bea52a3f25e48c61bc1e499ae138c1a
Andreas <andreas@massdriver.cloud>
2023-03-28 17:28:54 -0700

    remove unnecessary comment

--- src/bundle/publish.ts
+++ src/bundle/publish.ts
@@ -5,7 +5,6 @@ const run = async (): Promise<void> => {
   try {
     const command = `mass bundle build`
     await exec.exec(command)
-    // "must be set to any or unkown if specified"
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     core.setFailed(error.message)
