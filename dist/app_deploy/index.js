af1227459e2d25b48907ad9dd8fba4020c5cd8a2
Andreas <andreas@massdriver.cloud>
2024-02-14 17:52:23 +0000

    update dist

--- dist/app_deploy/index.js
+++ dist/app_deploy/index.js
@@ -25980,9 +25980,11 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
     const project = core.getInput("project");
     const env = core.getInput("env");
     const manifest = core.getInput("manifest");
+    const message = core.getInput("message", { required: false });
+    const messageFlag = message ? [`--message "${message}`] : [];
     try {
         const command = `mass app deploy ${project}-${env}-${manifest}`;
-        yield exec.exec(command);
+        yield exec.exec(command, messageFlag);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     }
     catch (error) {
