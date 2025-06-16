39591658649247e48872aa74f3ccd7684e10e79a
Christopher Hill <chris@massdriver.cloud>
2025-06-15 21:45:10 -0700

    Change mass bundle actions to use --build-directory flag (#249)
    * Change mass bundle actions to use --build-directory flag
    
    * switch back to --build-directory

--- src/bundle_publish.ts
+++ src/bundle_publish.ts
@@ -3,11 +3,10 @@ import * as exec from "@actions/exec"
 
 const run = async (): Promise<void> => {
   const buildDirectory = core.getInput("build-directory", {required: false})
-  const access = core.getInput("access", {required: false})
 
   try {
     const command = `mass bundle publish`
-    const args = [`--build-directory`, buildDirectory, `--access`, access]
+    const args = [`--build-directory`, buildDirectory]
     await exec.exec(command, args)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
