de093f44e9dfdbe0c2bca60625fa81f9309beed3
chrisghill <chris@massdriver.cloud>
2025-09-05 16:22:08 -0600

    Add '--fail-warnings' and '--skip-lint' flags to bundle publish

--- src/bundle_publish.ts
+++ src/bundle_publish.ts
@@ -3,10 +3,20 @@ import * as exec from "@actions/exec"
 
 const run = async (): Promise<void> => {
   const buildDirectory = core.getInput("build-directory", {required: false})
+  const failWarnings = core.getInput("fail-warnings") === "true"
+  const skipLint = core.getInput("skip-lint") === "true"
 
   try {
     const command = `mass bundle publish`
     const args = [`--build-directory`, buildDirectory]
+
+    if (failWarnings) {
+      args.push(`--fail-warnings`)
+    }
+    if (skipLint) {
+      args.push(`--skip-lint`)
+    }
+
     await exec.exec(command, args)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
