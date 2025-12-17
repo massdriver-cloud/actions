fe1f8283a77a52d59bb00addd6afbf76152799dd
chrisghill <chris@massdriver.cloud>
2025-12-16 17:01:00 -0700

    remove the --file flag from definition publish

--- src/definition_publish.ts
+++ src/definition_publish.ts
@@ -6,7 +6,7 @@ const run = async (): Promise<void> => {
 
   try {
     const command = `mass definition publish`
-    const args = [`--file`, file]
+    const args = [file]
     await exec.exec(command, args)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
